ddc1a3ef139fd4e3032d51a98f6d51b48ed3c3c7
Galperry <galperry2@gmail.com>
2021-01-28 21:18:02 +0200

    final and also less ugly version

--- solution.js
+++ solution.js
@@ -1,88 +1,72 @@
+var arr;
+let containerEl;
+class Task {
+    constructor(id, body, done = false) {
+        this.id = id;
+        this.body = body;
+        this.done = done;
+    }
+}
 window.onload = () => {
-    let arr;
-    arr = JSON.parse(localStorage.getItem("tasks"));
-    if (!arr) {
-        arr = [];
-    }
-    for (const object of arr) {
-        showTask(object);
-    }
-    document.querySelector("#todo-add").addEventListener("submit", function (e) {
-        e.preventDefault();
-        createTask();
-    });
-    class Task {
-        constructor(id, body) {
-            this.id = id;
-            this.body = body;
-        }
-    }
-    function getIDandIncrease() {
-        let countObj = Number(localStorage.getItem("countObj"));
-        if (!countObj) {
-            countObj = 1;
-        }
-        else {
-            countObj++;
-        }
-        localStorage.setItem("countObj", String(countObj));
-        return countObj;
-    }
-    function createTask() {
-        let textArea = document.querySelector("#todo-item").value;
-        let obj = new Task(getIDandIncrease(), textArea);
-        arr.push(obj);
-        localStorage.setItem('tasks', JSON.stringify(arr));
-        showTask(obj);
-    }
-    function showTask(obj) {
-        let containerEl = document.querySelector("#todo-list");
-        let taskDiv = document.createElement("div");
-        taskDiv.classList.add("todo-row");
-        taskDiv.classList.add("todo-item");
-        taskDiv.id = obj.id;
-        taskDiv.style.width = "85%";
-        taskDiv.innerText = `${obj.body}`;
-        let vBtn = document.createElement("button");
-        vBtn.innerText = "✓";
-        vBtn.style.color = "white";
-        vBtn.classList.add("todo-ok");
-        containerEl.prepend(taskDiv);
-        taskDiv.append(vBtn);
-        addListenerButton(vBtn);
-    }
-    function addListenerButton(btn) {
-        btn.addEventListener("click", function () {
-            this.parentNode.classList.add("done");
-            // for (let i =0 ; i < arr.length ; i++){
-            //     if (arr[i].id == this.parentNode.id){
-            //         arr.splice(i,1)
-            //         localStorage.setItem('memos', JSON.stringify(arr));
-            //         this.parentNode.remove();
-            //     }
-            // }
+    containerEl = document.querySelector("#todo-list");
+    (function initialize() {
+        arr = JSON.parse(localStorage.getItem("tasks"));
+        if (!arr)
+            arr = [];
+        for (const object of arr)
+            showTask(object);
+        document.querySelector("#todo-save").addEventListener("click", function (e) {
+            e.preventDefault();
+            createTask();
         });
-    }
-    document.querySelector("#todo-delall").addEventListener("click", function () {
-        localStorage.clear();
-        arr = [];
-        let containerEl = document.querySelector("#todo-list");
-        while (containerEl.firstChild) {
-            containerEl.removeChild(containerEl.firstChild);
-        }
-    });
-    document.querySelector("#todo-delcom").addEventListener("click", function () {
-        let allCompleted = document.querySelectorAll(".done");
-        for (let i = 0; i < arr.length; i++) {
-            for (let j = 0; j < allCompleted.length; j++) {
-                if (Number(arr[i].id) == Number(allCompleted[j].id)) {
-                    arr.splice(i, 1);
-                    localStorage.setItem('tasks', JSON.stringify(arr));
-                }
+        document.querySelector("#todo-delall").addEventListener("click", function () {
+            localStorage.clear();
+            arr = [];
+            while (containerEl.firstChild) {
+                containerEl.removeChild(containerEl.firstChild);
             }
-        }
-        for (var i = allCompleted.length - 1; i >= 0; i--) {
-            allCompleted[i].remove();
-        }
-    });
+        });
+        document.querySelector("#todo-delcom").addEventListener("click", function () {
+            let allCompleted = document.querySelectorAll(".done");
+            let newArr = [];
+            for (var i = allCompleted.length - 1; i >= 0; i--) {
+                allCompleted[i].parentNode.remove();
+            }
+            for (let obj of arr) {
+                if (!obj.done)
+                    newArr.push(obj);
+            }
+            localStorage.setItem("tasks", JSON.stringify(newArr));
+        });
+    })();
 };
+function addListenerButton(id) {
+    const parentDiv = document.getElementById(String(id));
+    parentDiv.children[0].classList.add("done");
+    arr.find((task) => task.id == id).done = true;
+    localStorage.setItem("tasks", JSON.stringify(arr));
+}
+function createTask() {
+    let textArea = document.querySelector("#todo-item")
+        .value;
+    let obj = new Task(getIDandIncrease(), textArea);
+    arr.push(obj);
+    localStorage.setItem("tasks", JSON.stringify(arr));
+    showTask(obj);
+}
+function getIDandIncrease() {
+    let countObj = Number(localStorage.getItem("countObj"));
+    !countObj ? (countObj = 1) : countObj++;
+    localStorage.setItem("countObj", String(countObj));
+    return countObj;
+}
+function showTask(obj) {
+    if (!obj.body)
+        return;
+    containerEl.innerHTML += `
+        <div id="${obj.id}" class="todo-row">
+            <div class="todo-item ${obj.done ? "done" : ""}">${obj.body}</div>
+            <input type="button" onclick="addListenerButton(${obj.id})" class="todo-ok" value="✓"/>
+        </div>
+    `;
+}
--- solution.ts
+++ solution.ts
@@ -1,113 +1,82 @@
-window.onload = () :void =>{
+var arr: Array<Task>;
+let containerEl: HTMLInputElement;
 
-    let arr:Array<Task>; 
-
-    arr = JSON.parse(localStorage.getItem("tasks"));
-    if (! arr){
-        arr=[]
-    }
-    for (const object of arr){
-        showTask(object)
-    }
-
-    document.querySelector("#todo-add").addEventListener("submit", function(e):void{
-       e.preventDefault();
-       createTask();
-    })
-
-    class Task {
-        public id:number; public body:string;
-        constructor (id:number,body:string){
-            this.id = id;
-            this.body = body;
-        }
-    }
-
-
-    function getIDandIncrease():number{
-        let countObj:number = Number(localStorage.getItem("countObj"));
-        if (!countObj) {
-            countObj= 1;
-        }
-        else{
-            countObj++
-        }
-        localStorage.setItem("countObj",String(countObj))
-        return countObj;
-    }
-
-
-    function createTask() :void{
-        let textArea:any = (<HTMLInputElement>document.querySelector("#todo-item")).value
-        let obj:Task = new Task (getIDandIncrease(), textArea)
-        arr.push(obj);
-        localStorage.setItem('tasks',JSON.stringify(arr))
-        showTask(obj)
-    }
-
-
-    function showTask (obj) :void{
-        let containerEl:HTMLInputElement = document.querySelector("#todo-list")
-    
-        let taskDiv:any = document.createElement("div")
-        taskDiv.classList.add("todo-row")
-        taskDiv.classList.add("todo-item")
-        taskDiv.id = obj.id;
-        taskDiv.style.width = "85%"
-        taskDiv.innerText = `${obj.body}`
-    
-        let vBtn:any = document.createElement("button")
-        vBtn.innerText = "✓"
-        vBtn.style.color = "white"
-        vBtn.classList.add("todo-ok")
-        
-        containerEl.prepend(taskDiv)
-        taskDiv.append(vBtn)
-        
-        addListenerButton(vBtn)
-    }
-
-    function addListenerButton(btn) :void{
-        btn.addEventListener("click", function(){
-            this.parentNode.classList.add("done")
-            // for (let i =0 ; i < arr.length ; i++){
-            //     if (arr[i].id == this.parentNode.id){
-            //         arr.splice(i,1)
-            //         localStorage.setItem('memos', JSON.stringify(arr));
-            //         this.parentNode.remove();
-            //     }
-            // }
-        })
-    }
-
-    document.querySelector("#todo-delall").addEventListener("click", function():void{
-        localStorage.clear();
-        arr=[];
-        let containerEl:HTMLInputElement = document.querySelector("#todo-list")
-        while (containerEl.firstChild) {
-            containerEl.removeChild(containerEl.firstChild);
-        }
-    })
-
-    document.querySelector("#todo-delcom").addEventListener("click", function () {
-        let allCompleted = document.querySelectorAll(".done")
-
-        for (let i = 0 ; i < arr.length ; i++){
-            for (let j = 0; j<allCompleted.length ; j++){
-                if (Number(arr[i].id) == Number(allCompleted[j].id)){
-                    arr.splice(i,1)
-                    localStorage.setItem('tasks', JSON.stringify(arr));
-                }
-            }
-        }
-        for (var i = allCompleted.length-1; i>=0;i--){
-            allCompleted[i].remove()
-        }
-    });
-
+class Task {
+  public id: number; public body: string; public done: boolean;
+  constructor(id: number, body: string, done: boolean = false) {
+    this.id = id;
+    this.body = body;
+    this.done = done;
+  }
 }
 
+window.onload = (): void => {
+    containerEl = document.querySelector("#todo-list");
+    
+    (function initialize(): void {
+        arr = JSON.parse(localStorage.getItem("tasks"));
+        if (!arr) arr = [];
+        for (const object of arr) showTask(object);
 
+        document.querySelector("#todo-save").addEventListener("click", function (e: Event): void {
+            e.preventDefault();
+            createTask();
+        });
+    
+        document.querySelector("#todo-delall").addEventListener("click", function (): void {
+            localStorage.clear();
+            arr = [];
+            while (containerEl.firstChild) {
+                containerEl.removeChild(containerEl.firstChild);
+            }
+        });
+  
+        document.querySelector("#todo-delcom").addEventListener("click", function () {
+            let allCompleted = document.querySelectorAll(".done");
+            let newArr = [];
+  
+            for (var i = allCompleted.length - 1; i >= 0; i--) {
+            (<HTMLInputElement>allCompleted[i].parentNode).remove();
+            }
+      
+            for (let obj of arr) {if (!obj.done) newArr.push(obj)}
+                localStorage.setItem("tasks", JSON.stringify(newArr));
+            });
 
+    })();
+};
 
+function addListenerButton(id:number): void {
+  const parentDiv = document.getElementById(String(id));
+  parentDiv.children[0].classList.add("done");
+  
+  arr.find((task) => task.id == id).done = true;
+  localStorage.setItem("tasks", JSON.stringify(arr));
+}
 
+function createTask(): void {
+    let textArea: any = (<HTMLInputElement>document.querySelector("#todo-item"))
+      .value;
+    let obj: Task = new Task(getIDandIncrease(), textArea);
+    arr.push(obj);
+    localStorage.setItem("tasks", JSON.stringify(arr));
+    showTask(obj);
+}
+
+function getIDandIncrease(): number {
+    let countObj: number = Number(localStorage.getItem("countObj"));
+    !countObj ? (countObj = 1) : countObj++;
+    localStorage.setItem("countObj", String(countObj));
+    return countObj;
+}
+
+function showTask(obj): void {
+    if (!obj.body) return;
+    
+    containerEl.innerHTML += `
+        <div id="${obj.id}" class="todo-row">
+            <div class="todo-item ${obj.done ? "done" : ""}">${obj.body}</div>
+            <input type="button" onclick="addListenerButton(${obj.id})" class="todo-ok" value="✓"/>
+        </div>
+    `;
+}
